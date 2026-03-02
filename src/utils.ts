1b49d54f5d902ec539fc6e2f0a5ae46517a5e13a
kahyou22 <kahyou222@gmail.com>
2026-03-03 01:58:36 +0900

    feat: 유틸에 padLeft 추가

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export const toInt = (num: string | number): number => {
   return ~~num;
 };
+
+export const padLeft = (str: string, length: number, char: string): string => {
+  return str.length >= length ? str : new Array(length - str.length + 1).join(char) + str;
+};
